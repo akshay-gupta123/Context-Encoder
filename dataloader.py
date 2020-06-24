c8b48ad0705d99ca7d7f4980d1361d567479ade2
akshay-gupta123 <56124489+akshay-gupta123@users.noreply.github.com>
2020-06-24 17:16:36 +0530

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -8,7 +8,7 @@ def get_dataset(args):
     x_train = (x_train - 127.5) / 255.0
     args.data_shape = x_train.shape[1:]
 
-    idx = np.random.randint(0, len(x_train), args.num_imgs)
+    idx = np.random.randint(0, len(x_train), args.num_img)
     validation = x_train[idx]
   
     train_ds = tf.data.Dataset.from_tensor_slices(x_train).shuffle(10000).batch(args.batch_size)
